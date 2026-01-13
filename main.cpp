994b1e1cff7dda42efbdb5586c4455e2c31acc4d
Daniel Wyrwał <daniel.wyrwal@gmail.com>
2026-01-13 13:17:23 +0100

    Add libmagic

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
-
 #include <iostream>
+#include <magic.h>
 
 int main() {
-
-  std::cout << "Hello, World!" << std::endl;
+  magic_t m = magic_open(MAGIC_MIME_TYPE);
+  magic_load(m, nullptr);
+  const char *mime = magic_file(m, "sw_d");
+  std::cout << mime << std::endl;
+  magic_close(m);
   return 0;
-  błąd
 }
